cdb9efade48fe90b2b1ce3826c38d236a049e205
Daniel Silverstone <dsilvers@digital-scurf.org>
2022-09-14 19:55:07 +0100

    (frontend): Augment role component with active status
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- frontend/src/components/role.rs
+++ frontend/src/components/role.rs
@@ -1,3 +1,4 @@
+use stylist::yew::{styled_component, use_style};
 use yew::prelude::*;
 
 use crate::utils::cache::{CacheEntry, ObjectCache};
@@ -5,13 +6,23 @@ use crate::utils::cache::{CacheEntry, ObjectCache};
 #[derive(Properties, PartialEq)]
 pub struct RoleProps {
     pub uuid: String,
+    pub active: Option<bool>,
     pub onclick: Option<Callback<()>>,
 }
 
-#[function_component(Role)]
+#[styled_component(Role)]
 pub fn render_role(props: &RoleProps) -> Html {
+    let role_style = use_style!(
+        r#"
+        display: inline-block;
+        width: 250px;
+        overflow-x: hidden;
+        "#
+    );
     let cache = use_context::<ObjectCache>().expect("No cache?");
     let role_data = cache.cached_role(&props.uuid);
+
+    let active = props.active.unwrap_or(false);
 
     let role_body = match &*role_data {
         CacheEntry::Pending => {
@@ -32,11 +43,35 @@ pub fn render_role(props: &RoleProps) -> Html {
         }
     };
 
+    let icon_class = if active {
+        classes! {"fa-solid", "fa-circle-user"}
+    } else {
+        classes! {"fa-regular", "fa-circle-user"}
+    };
+
     let role_body = html! {
         <span class={"icon-text"}>
-            <span class={"icon"}><i class={"fa-regular fa-circle-user"}/></span>
+            <span class={"icon"}><i class={icon_class}/></span>
             {role_body}
         </span>
+    };
+
+    let role_classes = {
+        let mut ret = if active {
+            classes!(
+                "box",
+                "has-background-primary-light",
+                "is-shadowless",
+                "p-1"
+            )
+        } else {
+            classes!("block")
+        };
+        ret.push(role_style);
+        if props.onclick.is_some() {
+            ret.push(classes! {"is-clickable"});
+        }
+        ret
     };
 
     if let Some(cb) = &props.onclick {
@@ -44,14 +79,14 @@ pub fn render_role(props: &RoleProps) -> Html {
         let cb = cb.clone();
         let onclick = Callback::from(move |_| cb.emit(()));
         html! {
-            <div class={"block"} onclick={onclick}>
+            <div class={role_classes} onclick={onclick}>
                 {role_body}
             </div>
         }
     } else {
         // No onclick, so just show
         html! {
-            <div class={"block"}>
+            <div class={role_classes}>
                 {role_body}
             </div>
         }
