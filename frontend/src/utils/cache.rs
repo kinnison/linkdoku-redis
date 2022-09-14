7097d2d886ce90006df262b7895dbd302cbcb1b5
Daniel Silverstone <dsilvers@digital-scurf.org>
2022-09-14 11:07:11 +0100

    (frontend): Improve cache with expiry
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- frontend/src/utils/cache.rs
+++ frontend/src/utils/cache.rs
@@ -1,8 +1,9 @@
 //! Cache provision for Linkdoku
 //!
 
-use std::{collections::HashMap, future::Future};
+use std::{collections::HashMap, future::Future, time::Duration};
 
+use js_sys::Date;
 use linkdoku_common::RoleData;
 use serde::{de::DeserializeOwned, Serialize};
 use serde_json::Value;
@@ -11,14 +12,22 @@ use yew_hooks::{use_async_with_options, use_map, UseAsyncHandle, UseAsyncOptions
 
 use crate::components::core::{make_api_call, use_api_url, APIError, ReqwestClient, NO_BODY};
 
+const ROLE_CACHE_LIFETIME: Duration = Duration::from_secs(60 * 5); // Five minute cache for roles
+
 #[derive(Properties, PartialEq)]
 pub struct ObjectCacheProviderProps {
     pub children: Children,
 }
 
 #[derive(Clone, PartialEq)]
+pub struct CacheEntryExpiry {
+    expires: f64,
+    entry: Value,
+}
+
+#[derive(Clone, PartialEq)]
 pub struct ObjectCache {
-    cache: UseMapHandle<String, Value>,
+    cache: UseMapHandle<String, CacheEntryExpiry>,
 }
 
 #[derive(Clone, PartialEq)]
@@ -42,22 +51,38 @@ pub fn object_cache_provider(props: &ObjectCacheProviderProps) -> Html {
 }
 
 impl ObjectCache {
-    pub fn add_object<V: Serialize>(&self, uuid: &str, value: V) {
+    pub fn add_object<V: Serialize>(&self, uuid: &str, lifetime: Duration, value: V) {
         let val = serde_json::to_value(value).expect("Unable to serialize value!");
         gloo::console::log!(format!("Adding cache entry for {}", uuid));
-        self.cache.insert(uuid.to_string(), val);
+        self.cache.insert(
+            uuid.to_string(),
+            CacheEntryExpiry {
+                expires: Date::now() + (lifetime.as_secs_f64() * 1000.0),
+                entry: val,
+            },
+        );
     }
 
     pub fn get<V: DeserializeOwned>(&self, uuid: &str) -> Option<V> {
         if let Some(value) = self.cache.current().get(uuid) {
-            gloo::console::log!(format!("Retrieved {} from cache", uuid));
-            serde_json::from_value(value.clone()).ok()
+            if Date::now() <= value.expires {
+                gloo::console::log!(format!("Retrieved {} from cache", uuid));
+                serde_json::from_value(value.entry.clone()).ok()
+            } else {
+                gloo::console::log!(format!("Entry for {} expired", uuid));
+                None
+            }
         } else {
             None
         }
     }
 
-    pub fn use_cached_value<F, T, E>(&self, uuid: &str, fetcher: F) -> UseAsyncHandle<Option<T>, E>
+    pub fn use_cached_value<F, T, E>(
+        &self,
+        uuid: &str,
+        lifetime: Duration,
+        fetcher: F,
+    ) -> UseAsyncHandle<Option<T>, E>
     where
         T: Clone + Serialize + DeserializeOwned + 'static,
         E: Clone + 'static,
@@ -73,7 +98,7 @@ impl ObjectCache {
                 };
                 let value = fetcher.await?;
                 match &value {
-                    Some(value) => cache.add_object(&uuid, value),
+                    Some(value) => cache.add_object(&uuid, lifetime, value),
                     None => {
                         cache.cache.remove(&uuid);
                     }
@@ -85,12 +110,13 @@ impl ObjectCache {
     }
 
     pub fn cached_role(&self, uuid: &str) -> UseStateHandle<CacheEntry<RoleData>> {
+        let lifetime = ROLE_CACHE_LIFETIME;
         let cache = use_context::<ObjectCache>().expect("Cache not extant!");
         let state = use_state_eq(|| CacheEntry::Pending);
         let client = use_context::<ReqwestClient>().expect("No API client");
         let async_handle: UseAsyncHandle<Option<RoleData>, crate::components::core::APIError> = {
             let api_url = use_api_url(&format!("/role/get/{}", uuid));
-            cache.use_cached_value(uuid, async move {
+            cache.use_cached_value(uuid, lifetime, async move {
                 let out: Option<RoleData> =
                     make_api_call(client, api_url.as_str(), None, NO_BODY).await?;
                 Ok(out)
