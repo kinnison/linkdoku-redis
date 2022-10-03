641c5f589b66afe29b5dd5bc0f97f14d90a34998
Daniel Silverstone <dsilvers@digital-scurf.org>
2022-10-03 15:02:13 -0400

    (common): Add puzzle related types
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum BackendLoginStatus {
@@ -29,4 +30,74 @@ pub struct RoleData {
     pub short_name: String,
     pub display_name: String,
     pub bio: String,
+}
+
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Visibility {
+    #[default]
+    Restricted,
+    Public,
+    Published,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Puzzle {
+    pub uuid: String,
+    pub owner: String,
+    pub display_name: String,
+    pub short_name: String,
+    pub visibility: Visibility,
+    pub visibility_changed: Option<String>,
+    pub states: Vec<PuzzleState>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub enum Rating {
+    Easier,
+    Easy,
+    #[default]
+    Normal,
+    Hard,
+    Harder,
+}
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct PuzzleState {
+    pub description: String,
+    pub setter_rating: Rating,
+    pub data: PuzzleData,
+    pub visibility: Visibility,
+    pub visibility_changed: Option<String>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub enum PuzzleData {
+    #[default]
+    Nothing,
+    URLs(Vec<UrlEntry>),
+    Pack(Vec<String>),
+    FPuzzles(Value),
+}
+
+#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+pub struct UrlEntry {
+    pub title: String,
+    pub url: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub enum CreatePuzzleResponse {
+    /// Successful create, contained string is puzzle UUID
+    Success(String),
+    /// Failure because user is not logged in
+    NotLoggedIn,
+    /// Failure because provided puzzle contained a UUID
+    FailedUUIDSupplied,
+    /// Invalid role supplied as owner
+    InvalidOwnerRole,
+    /// Invalid state vector, must be exactly one entry
+    InvalidStateVector,
+    /// Invalid visibility data provided
+    InvalidVisiblityData,
+    /// Something went wrong in the database layer
+    DatabaseFailure(String),
 }
