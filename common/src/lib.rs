0564f24f88b65ba6190c7dc167330589d540a29b
Daniel Silverstone <dsilvers@digital-scurf.org>
2022-10-03 22:28:05 -0400

    (common): A better puzzle rating, and formatter for puzzle creation responses
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -51,15 +53,64 @@ pub struct Puzzle {
     pub states: Vec<PuzzleState>,
 }
 
-#[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Rating {
-    Easier,
+    Tutorial,
+    Beginner,
     Easy,
     #[default]
-    Normal,
+    Regular,
     Hard,
-    Harder,
+    VeryHard,
 }
+
+impl Rating {
+    pub fn values() -> &'static [Rating] {
+        &[
+            Self::Tutorial,
+            Self::Beginner,
+            Self::Easy,
+            Self::Regular,
+            Self::Hard,
+            Self::VeryHard,
+        ]
+    }
+
+    pub fn from_value(v: &str) -> Self {
+        match v {
+            "tutorial" => Self::Tutorial,
+            "beginner" => Self::Beginner,
+            "easy" => Self::Easy,
+            "regular" => Self::Regular,
+            "hard" => Self::Hard,
+            "veryhard" => Self::VeryHard,
+            _ => Self::Regular,
+        }
+    }
+
+    pub fn value(self) -> &'static str {
+        match self {
+            Rating::Tutorial => "tutorial",
+            Rating::Beginner => "beginner",
+            Rating::Easy => "easy",
+            Rating::Regular => "regular",
+            Rating::Hard => "hard",
+            Rating::VeryHard => "veryhard",
+        }
+    }
+
+    pub fn title(self) -> &'static str {
+        match self {
+            Rating::Tutorial => "Tutorial puzzle",
+            Rating::Beginner => "Good for beginners",
+            Rating::Easy => "Generally easy",
+            Rating::Regular => "Regular difficulty",
+            Rating::Hard => "Puzzle is hard",
+            Rating::VeryHard => "Puzzle is very hard",
+        }
+    }
+}
+
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 pub struct PuzzleState {
     pub description: String,
@@ -100,4 +151,18 @@ pub enum CreatePuzzleResponse {
     InvalidVisiblityData,
     /// Something went wrong in the database layer
     DatabaseFailure(String),
+}
+
+impl Display for CreatePuzzleResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            CreatePuzzleResponse::Success(uuid) => write!(f, "Ok({})", uuid),
+            CreatePuzzleResponse::NotLoggedIn => write!(f, "Not logged in"),
+            CreatePuzzleResponse::FailedUUIDSupplied => write!(f, "Unexpected UUID in input"),
+            CreatePuzzleResponse::InvalidOwnerRole => write!(f, "Invalid owner role in input"),
+            CreatePuzzleResponse::InvalidStateVector => write!(f, "Invalid state vector"),
+            CreatePuzzleResponse::InvalidVisiblityData => write!(f, "Invalid visibility data"),
+            CreatePuzzleResponse::DatabaseFailure(e) => write!(f, "{}", e),
+        }
+    }
 }
